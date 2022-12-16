fa5e5d3f07e4f948964612595014faa136d72c71
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2022-12-16 17:16:33 +0500

    register

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Quiz, Question, Option
+from .models import Quiz, Question, Option, Topic, Result, ResultDetail, Student
 # Register your models here.
 admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(Option)
+admin.site.register(Topic)
+admin.site.register(Result)
+admin.site.register(ResultDetail)
+admin.site.register(Student)
