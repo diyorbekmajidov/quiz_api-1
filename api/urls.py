2fa5c169fb253d360f30e3ce864785817a7d7373
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2022-12-20 15:39:45 +0500

    student url studentget url

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import QuizListView, QuestionListView, TopicListView,OptionListViev
+from .views import QuizListView, QuestionListView, TopicListView,OptionListViev,StudentListView,StudentListView1,UpdaterStudent
 
 urlpatterns = [
     path('quiz/', QuizListView.as_view()),
     path('quiz/<int:pk>/', QuestionListView.as_view()),
+    path('question/', QuestionListView.as_view()),
     path('topic/<int:pk>/', TopicListView.as_view()),
-    path('option/', OptionListViev.as_view())
+    path('option/', OptionListViev.as_view()),
+    path('student/', StudentListView.as_view()),
+    path('studentget/<int:chatid>/', StudentListView1.as_view()),
+    path('updaterstudent/<int:pk>/', UpdaterStudent.as_view()),
 ]
